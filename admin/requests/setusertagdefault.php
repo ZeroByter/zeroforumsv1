48983b823b75d41de43ccbe724af04de71a6b04b
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 11:44:21 +0300

    added create_log

--- admin/requests/setusertagdefault.php
+++ admin/requests/setusertagdefault.php
@@ -3,10 +3,13 @@
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
     include("../../phpscripts/navigatebar.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["id"])){
         // TODO: Make sure user has permissions to do this!
+        $usertag = get_usertag_by_id($_POST["id"]);
         make_usertag_default($_POST["id"]);
+        create_log(get_current_account()->username . " made the usertag '$usertag->name' the default usertag");
         echo "success";
     }else{
         echo "error:missing information";
