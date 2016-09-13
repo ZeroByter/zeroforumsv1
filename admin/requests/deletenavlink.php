fedb3e1ac3e44643a15f04353821b9a32795844e
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 11:41:48 +0300

    added create_log

--- admin/requests/deletenavlink.php
+++ admin/requests/deletenavlink.php
@@ -3,11 +3,14 @@
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
     include("../../phpscripts/navigatebar.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["id"])){
         // TODO: Make sure user has permissions to do this!
-        if(get_navlink_by_id($_POST["id"])->candelete){
+        $navlink = get_navlink_by_id($_POST["id"]);
+        if($navlink->candelete){
             remove_navbar_link($_POST["id"]);
+            create_log(get_current_account()->username . " deleted the navigation link '$navlink->text'");
             echo "success";
         }else{
             echo "error:cant delete this link!";
