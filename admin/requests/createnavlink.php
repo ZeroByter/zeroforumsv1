099e94afac0875c7d6690a365e80f7caa05b8395
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 11:33:31 +0300

    added create_log

--- admin/requests/createnavlink.php
+++ admin/requests/createnavlink.php
@@ -3,10 +3,12 @@
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
     include("../../phpscripts/navigatebar.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["text"]) && isset($_POST["link"]) && isset($_POST["listorder"]) && isset($_POST["canview"])){
         // TODO: Make sure user has permissions to do this!
         add_navbar_link($_POST["text"], $_POST["link"], $_POST["listorder"], $_POST["canview"]);
+        create_log(get_current_account()->username . " created the navigation link '".$_POST["text"]."' with link '".$_POST["link"]."'");
         echo "success";
     }else{
         echo "error:missing information";
