8f8ffdab0cbb6d94c32e5cefe28d81d6370c49d2
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 11:40:04 +0300

    added create_log

--- admin/requests/createusertag.php
+++ admin/requests/createusertag.php
@@ -3,10 +3,12 @@
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
     include("../../phpscripts/navigatebar.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["name"]) && isset($_POST["listorder"]) && isset($_POST["isstaff"])){
         // TODO: Make sure user has permissions to do this!
         add_usertag($_POST["name"], ";", $_POST["listorder"], false, $_POST["isstaff"]);
+        create_log(get_current_account()->username . " created the usertag '".$_POST["name"]."'");
         echo "success";
     }else{
         echo "error:missing information";
