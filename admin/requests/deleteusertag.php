220014116a474ddfea700525a02a516f73512357
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 11:43:00 +0300

    added create_log

--- admin/requests/deleteusertag.php
+++ admin/requests/deleteusertag.php
@@ -3,14 +3,17 @@
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
     include("../../phpscripts/navigatebar.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["id"])){
         // TODO: Make sure user has permissions to do this!
-        if(get_usertag_by_id($_POST["id"])->isdefault){
+        $usertag = get_usertag_by_id($_POST["id"]);
+        if($usertag->isdefault){
             echo "error:cant delete default usertag! link!";
         }else{
             delete_usertag($_POST["id"]);
             kick_all_users_from_usertag($_POST["id"]);
+            create_log(get_current_account()->username . " deleted the usertag '$usertag->name'");
             echo "success";
         }
     }else{
