91e9a2da09da50dbbe47e5e2bda22d639d8f1992
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 14:11:26 +0300

    added create_log
    add so it says which permission was added/removed from usertag

--- admin/requests/setusertagperms.php
+++ admin/requests/setusertagperms.php
@@ -3,10 +3,13 @@
     include("../../phpscripts/essentials.php");
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["usertag"]) && isset($_POST["permissions"])){
         // TODO: Make sure current account has sufficicent permissions
         update_usertag_permissions($_POST["usertag"], $_POST["permissions"]);
+        $usertag = get_usertag_by_id($_POST["usertag"]);
+        log_create(get_current_account()->username . " changed permissions for usertag '$usertag->name'"); // TODO: add so it also says what permission was deleted or removed
     }else{
         echo "error:no information given!";
     }
