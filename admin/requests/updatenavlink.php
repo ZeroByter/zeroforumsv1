8ed7776922eb31708bf0158c144d21fd05f82ca7
Rom Levtov <romgamesrom@gmail.com>
2016-09-13 14:14:33 +0300

    added create_log

--- admin/requests/updatenavlink.php
+++ admin/requests/updatenavlink.php
@@ -3,10 +3,13 @@
     include("../../phpscripts/accounts.php");
     include("../../phpscripts/usertags.php");
     include("../../phpscripts/navigatebar.php");
+    include("../../phpscripts/logs.php");
 
     if(isset($_POST["id"]) && isset($_POST["text"]) && isset($_POST["link"]) && isset($_POST["listorder"]) && isset($_POST["canview"])){
         // TODO: Make sure user has permissions to do this!
-        if(get_navlink_by_id($_POST["id"])->canedit){
+        $navlink = get_navlink_by_id($_POST["id"]);
+        if($navlink->canedit){
+            create_log(get_current_account()->username . " updated navigation link '$navlink->text' with new properties: text: ".$_POST["text"].", link: ".$_POST["link"]);
             update_navbar_link($_POST["id"], $_POST["text"], $_POST["link"], $_POST["canview"], $_POST["listorder"]);
             echo "success";
         }else{
